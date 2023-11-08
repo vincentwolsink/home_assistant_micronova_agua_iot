28a2d7c87bdc18f8e6caf9d1c68bfb6f7cd03e7d
jipem01 <46441154+jipem01@users.noreply.github.com>
2023-11-08 22:14:28 +0100

    Nobis hybrid stove sensors implementation
    Fix real pellet power sensor on hybrid stoves
    Add pellets and wood power controls
    Add wood reserve indication

--- custom_components/aguaiot/const.py
+++ custom_components/aguaiot/const.py
@@ -28,6 +28,11 @@ from dataclasses import dataclass
 
 
 @dataclass
+class AguaIOTBinarySensorEntityDescription(BinarySensorEntityDescription):
+    force_enabled: bool = False
+
+
+@dataclass
 class AguaIOTSensorEntityDescription(SensorEntityDescription):
     force_enabled: bool = False
 
@@ -51,10 +56,17 @@ PLATFORMS = [
 UPDATE_INTERVAL = 60
 
 BINARY_SENSORS = (
-    BinarySensorEntityDescription(
+    AguaIOTBinarySensorEntityDescription(
         key="ris_pellet_ris_get",
         name="Pellets Depleted",
         device_class=BinarySensorDeviceClass.PROBLEM,
+    ),
+    AguaIOTBinarySensorEntityDescription(
+        key="popup_riserva_wood_get",
+        name="Wood reserve",
+        icon="mdi:fire-alert",
+        device_class=BinarySensorDeviceClass.PROBLEM,
+        force_enabled=True,
     ),
 )
 
@@ -92,11 +104,21 @@ SENSORS = (
     ),
     AguaIOTSensorEntityDescription(
         key="real_power_get",
-        name="Real Power",
+        name="Real Pellet Power",
         icon="mdi:gauge",
         native_unit_of_measurement=None,
         state_class=None,
         device_class=None,
+        force_enabled=True,
+    ),
+    AguaIOTSensorEntityDescription(
+        key="real_power_wood_get",
+        name="Real Wood Power",
+        icon="mdi:gauge",
+        native_unit_of_measurement=None,
+        state_class=None,
+        device_class=None,
+        force_enabled=True,
     ),
 )
 
@@ -129,6 +151,16 @@ NUMBERS = (
         native_step=1,
         native_unit_of_measurement=UnitOfTemperature.CELSIUS,
         device_class=NumberDeviceClass.TEMPERATURE,
+    ),
+    NumberEntityDescription(
+        key="power_set",
+        name="Pellet Power",
+        native_step=1,
+    ),
+    NumberEntityDescription(
+        key="power_wood_set",
+        name="Wood Power",
+        native_step=1,
     ),
 )
 
