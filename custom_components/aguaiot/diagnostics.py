ddbd8bbef333e26288f6892f9771889b3b681fc6
Vincent Wolsink <ik@vincentwolsink.nl>
2025-11-24 20:14:45 +0100

    Fix diagnostics

--- custom_components/aguaiot/diagnostics.py
+++ custom_components/aguaiot/diagnostics.py
@@ -7,7 +7,6 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
 
 TO_REDACT = {
     CONF_PASSWORD,
@@ -16,10 +15,11 @@ TO_REDACT = {
 
 
 async def async_get_config_entry_diagnostics(
-    hass: HomeAssistant, entry: ConfigEntry
+    hass: HomeAssistant, config_entry: ConfigEntry
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
-    agua = hass.data[DOMAIN][entry.entry_id]["agua"]
+    coordinator = config_entry.runtime_data
+    agua = coordinator.agua
 
     devices = {}
     for device in agua.devices:
@@ -28,6 +28,6 @@ async def async_get_config_entry_diagnostics(
             devices[device.name][reg] = device.get_register(reg)
 
     return {
-        "entry": async_redact_data(entry.as_dict(), TO_REDACT),
+        "entry": async_redact_data(config_entry.as_dict(), TO_REDACT),
         "devices": devices,
     }
